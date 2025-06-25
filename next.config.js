c182c13d7af7c62ba4707576bbe06cc60c82d952
Anant <85330429+anant-harryfan@users.noreply.github.com>
2025-06-25 18:22:00 +0530

    Update next.config.js output export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+
 const nextConfig = {
+    output: "export",  // <=== enables static exports
+  reactStrictMode: true,
   images: {
     domains: [
       'images.unsplash.com',
